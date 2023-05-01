7b32f20a9125f702c6f03a87759cf86d551885a0
Hripsime <grigoryanhripsime18@gmail.com>
2023-05-02 01:20:03 +0400

    This program uses your input to perform arithmetic operations (addition, subtraction, multiplication, or division). If something goes wrong during the calculation, the program catches the exception. Additionally, when you run the code, a log file is created for recording any events or errors that occur.

--- calculator.py
+++ calculator.py
@@ -1,5 +1,14 @@
 #! /usr/bin/python3
 
+'''
+Exception types that we can use to catch them and get logs:
+    deviding to zero
+    getting not a numeric value as a number
+    getting operation's name that doesn't exist / for this we need to raise an exception
+'''
+
+import logging
+
 class Calculator:
 	def add(self, n1, n2):
 		return n1 + n2
@@ -13,20 +22,44 @@ class Calculator:
 	def div(self, n1, n2):
 		return n1 / n2
 
+logging.basicConfig(filename = "logging_file.log", filemode = "w+", format = '%(asctime)s %(levelname)s:%(message)s', level = 10)
 
 
-n1 = int(input("Enter first number: "))
-n2 = int(input("Enter secont number: "))
+try:
+    n1 = int(input("Enter first number: "))
+    logging.info("n1 was successfully added")
+except ValueError:
+    logging.error("Trouble with inputting n1")
+    exit(1)
+
+try:
+    n2 = int(input("Enter second number: "))
+    logging.info("n2 was successfully added")
+except ValueError:
+    logging.error("Trouble with inputting n2")
+    exit(1)
+
 operation = input("Enter operation type(add/sub/mult/div): ")
 
 calculator = Calculator()
-match operation:
-	case "add":
-		print(calculator.add(n1, n2))
-	case "sub":
-		print(calculator.sub(n1, n2))
-	case "mult":
-		print(calculator.mult(n1, n2))
-	case "div":
-		print(calculator.div(n1, n2))
-	
+
+if operation == "add":
+    print(calculator.add(n1, n2))
+    logging.info("Successfully added %s to %s", n1, n2)
+elif operation == "sub":
+    print(calculator.sub(n1, n2))
+    logging.info("Successfully subtracted %s to %s", n1, n2)
+elif operation == "mult":
+    print(calculator.mult(n1, n2))
+    logging.info("Successfully multiplied %s to %s", n1, n2)
+elif operation == "div":
+    try:
+        print(calculator.div(n1, n2))
+        logging.info("Successfully divided %s by %s", n1, n2)
+    except ZeroDivisionError:
+        logging.error("Division by zero")
+        exit(1)
+else:
+    logging.error("Wrong inputed operation name")
+
+logging.info("Programm was successfully ended.")
