15ad84408e6662a284db77197082e1e41c0315be
grigoryanhripsime <108063456+grigoryanhripsime@users.noreply.github.com>
2023-05-08 07:28:08 -0700

    Update Bot.java

--- Java/Tictactoe/game/Bot.java
+++ Java/Tictactoe/game/Bot.java
@@ -1,7 +1,20 @@
-package VMware_practic_exercises.Java.Tictactoe.game;
+package Tictactoe.game;
+import java.lang.Math;
 public class Bot extends Player {
+    public enum Strategy {
+        Beginner ,
+        Intermediate,
+        Advanced
+    };
+    private Strategy str;
     public Bot(char o) {
+        super("Bot");
         this.marker = o;
+    }
+
+    public Bot(Strategy strategy) {
+        super('O', "Bot");
+        str = strategy;
     }
 
     boolean makeMove(String move, Board board) {
@@ -16,8 +29,20 @@ public class Bot extends Player {
     }
     String getMove(Board board) {
         String move;
-
-        //Emergency
+        if (str.equals(Strategy.Beginner)) {
+            while (true) {
+                int numx = (int) (Math.random() * 3);
+                int numy = (int) (Math.random() * 3);
+                move = "" + numx + numy;
+                System.out.println(move);
+                if (makeMove(move, board)) {
+                    System.out.println(move);
+                    return move;
+                }
+            }
+        }
+        if (str.equals(Strategy.Intermediate)) {
+            //Emergency
             char[][] game_board = board.getBoard();
             if (game_board[1][1] == 'X') {
                 if (game_board[0][0] == 'X') {
@@ -78,12 +103,42 @@ public class Bot extends Player {
                 }
             }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                move = "" + i + j;
-                System.out.println(move);
-                if (makeMove(move, board)) return move;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    move = "" + i + j;
+                    System.out.println(move);
+                    if (makeMove(move, board)) return move;
+                }
             }
+        }
+        if (str.equals(Strategy.Advanced)) {
+            //checking for bot's winning position
+//            
+//            Board board_copy = new Board(board);
+//            char[][] game_board = board_copy.getBoard();
+//            for (int i = 0; i < 3; i++) {
+//                for (int j = 0; j < 3; j++) {
+//                    move = "" + i + j;
+//                    if (makeMove(move, board_copy)) {
+//                        if (board.isWinner('O')) {
+//                            makeMove(move, board);
+//                            return move;
+//                        }
+//                    }
+//                }
+//            }
+//            while (true) {
+//                int numx = (int) (Math.random() * 3);
+//                int numy = (int) (Math.random() * 3);
+//                move = "" + numx + numy;
+//                System.out.println(move);
+//                if (makeMove(move, board)) {
+//                    System.out.println(move);
+//                    return move;
+//                }
+//            }
+//        }
+            return null;
         }
         return null;
     }
