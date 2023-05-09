d5fbbe40689163de6cba19b4a2fb677a2e62983b
grigoryanhripsime <108063456+grigoryanhripsime@users.noreply.github.com>
2023-05-09 20:16:43 +0400

    Deleted a Game(Board board) constructor, now there are two constructors, one for two players, second for playing with computer. Added condition if you play with bot or with another player. There Was a problem, that at the end of the game program was printing two boards, I also fixed that problem(when it was checking for result in if and it's not null, program call same function again, and for that we get two prints)

--- Java/Tictactoe/game/Game.java
+++ Java/Tictactoe/game/Game.java
@@ -1,28 +1,24 @@
-package Tictactoe.game;
+package VMware_practic_exercises.Java.Tictactoe.game;
 import java.util.Scanner;
 public class Game {
     private final Player player1;
-    private final Player player2; //Bot
+    private final Player player2;
     private final Board board;
 
-    public Game() {
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        this.player1 = new Player(name);
-        this.player2 = new Player('O', "Bot");
+    //when player choose to play with computer
+    public Game(Player player, Bot.Strategy strategy) {
+        System.out.println("You are playing with a bot, let's start!");
+        this.player1 = player;
+        this.player2 = new Bot(strategy);
         this.board = new Board();
     }
 
-//    public Game(Board board){
-//        this.player1 = new Player();
-//        this.player2 = new Player('O', name);
-//        this.board = board;
-//    }
-
-    public Game(Board board, Player player1, Player player2){
+    //when player choose playing with friend
+    public Game(Player player1, Player player2){
+        System.out.println("Player1\n" + player1 + "\nPlayer2\n" + player2);
         this.player1 = player1;
         this.player2 = player2;
-        this.board = board;
+        this.board = new Board();
     }
 
     public String playGame() throws Exception {
@@ -34,52 +30,69 @@ public class Game {
         //    if the move was not submitted (invalid move) then prints appropriate message and asks to make a move again from the same player
         // Keeps doing the above steps until one of the players wins, or the board gets full and no one wins
         // At the end this method should print the final result of the board and return the marker of the winner or the word "Tie" if no one wins
-        String move;
+        String move, res;
         do {
-            //getting player's move
-            while (true) {
-                board.printBoard();
-                System.out.println("Player " + player1.name);
-                move = player1.getMove(board);
-                try {
-                    board.submitMove(move, player1.getMarker());
-                    break;
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-            if (getResult() != null) {
-                return getResult();
-            }
-
-
-            //Bot
-            board.printBoard();
-            System.out.println("Bot");
-            player2.getMove(board);
-            if (getResult() != null) {
-                return getResult();
-            }
-            //Player2
-            /*
-            while (true) {
-                board.printBoard();
-                System.out.println("Player2");
-                move = player2.getMove(board);
-                try {
-                    board.submitMove(move, player2.getMarker());
-                    break;
-                }
-                catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-                finally {
-                    if (getResult() != null) {
-                        return getResult();
+            if (player2 instanceof Bot) {
+                //getting player's move
+                while (true) {
+                    board.printBoard();
+                    System.out.println("Player " + player1.name);
+                    move = player1.getMove(board);
+                    try {
+                        board.submitMove(move, player1.getMarker());
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
                     }
                 }
+                res = getResult();
+                if (res != null) {
+                    return res;
+                }
+                //Bot
+                board.printBoard();
+                System.out.println("Bot");
+                player2.getMove(board);
+                res = getResult();
+                if (res != null) {
+                    return res;
+                }
             }
-             */
+            else {
+                //getting player1's move
+                while (true) {
+                    board.printBoard();
+                    System.out.println("Player " + player1.name);
+                    move = player1.getMove(board);
+                    try {
+                        board.submitMove(move, player1.getMarker());
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+                res = getResult();
+                if (res != null) {
+                    return res;
+                }
+
+                //getting player2's move
+                while (true) {
+                    board.printBoard();
+                    System.out.println("Player " + player2.name);
+                    move = player2.getMove(board);
+                    try {
+                        board.submitMove(move, player2.getMarker());
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+                res = getResult();
+                if (res != null) {
+                    return res;
+                }
+            }
         } while(getResult() == null);
 
         return getResult();
