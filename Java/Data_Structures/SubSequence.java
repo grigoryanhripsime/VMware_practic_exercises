e3a6c1ebc4ed2863ad8c495f62ac816afd286340
grigoryanhripsime <108063456+grigoryanhripsime@users.noreply.github.com>
2023-05-22 06:56:17 -0700

    Update SubSequence.java

--- Java/Data_Structures/SubSequence.java
+++ Java/Data_Structures/SubSequence.java
@@ -29,3 +29,46 @@ public class SubSequence {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+public class SubSequence {
+    public static int[][] subSeq(String a, String b) {
+        int[][] matrix = new int[b.length()+1][a.length()+1];
+        for (int i = 1; i < a.length()+1; i++) {
+            for (int j = 1; j < b.length()+1; j++) {
+                if (a.charAt(i-1) == b.charAt(j-1)) {
+                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                }
+                else {
+                    if (matrix[i][j - 1] > matrix[i - 1][j]) {
+                        matrix[i][j] = matrix[i][j - 1];
+                    }
+                    else {
+                        matrix[i][j] = matrix[i - 1][j];
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = subSeq("BDADCABB", "CBAADBAB");
+        for (int[] i : matrix) {
+            for (int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
